dfe5d4d65920f3febbd3b088c73d2c4de0798c2e
b3717898 <b3717898@163.com>
2018-04-08 17:31:52 +0800

    Update CheckBillTask.java

--- sparkworker/src/main/java/com/rydeen/framework/bigdata/spark/checkbill/CheckBillTask.java
+++ sparkworker/src/main/java/com/rydeen/framework/bigdata/spark/checkbill/CheckBillTask.java
@@ -26,30 +26,7 @@ public class CheckBillTask {
     private String hdfsPrefixUrl = "hdfs://10.30.5.40:9000/SPFR/POC/";
     private String logPrefixUrl = "/home/spfr/poc/result/";
     private final Pattern SEMI = Pattern.compile(";");
-    //boh's regex
-    //CHS053;2018-03-07 00:00:00.000;2018-03-24 10:53:05.000;3030;
-    // {E3EAAB30-6D82-4D52-A0B3-37F58E9ECC72};2;3;微信顾客支付;NULL;67.5000;67.5000;
-    // tendered;3;NULL;CHS053-20180324-3030-4TC8po40567-USERPAY;
-    // CHS053-20180324-3030-4TC8po40567;5304;NULL;NULL;NULL;SchDaemon;2018-03-24 16:24:27.000;
-    // SchDaemon;2018-03-24 16:24:27.000
-    //ali:13,wx:15
-    //CHS053;2018-03-07 00:00:00.000;2018-03-24 10:52:47.000;5006;{64AAF808-2A00-4900-9AA3-FB8C61A0FBA0};
-    // 2;3;手机支付宝支付;NULL;115.0000;115.0000;tendered;5;NULL;2018-03-07AFPCHS0531521859946231046658;NULL;
-    // 333;NULL;NULL;NULL;SchDaemon;2018-03-24 16:24:27.000;SchDaemon;2018-03-24 16:24:27.000
-
-//    ALIPAY：37,38,40
-//    2088911787545094;ZGZ368;;;2018-03-24;1;82781;2018-03-24 08:37:16;2018032421001004710567116642;
-// *辉(152****7800);14.50;;支付宝;0.00;;支付宝;0.00;;0.00;0.00;14.50;0.00;14.50;37693f1238ff4225b97e08ac0e20369c;
-// 0.00;西广场;KFC;86024;QGD;QGD;肯德基;郑州肯德基有限公司;肯德基青岛市场;青岛财务;N;0.00;2018-03-07;
-// 2018-03-07AFPZGZ3681521851827812033726;2088802656589715;2;86890024;郑州肯德基有限公司
-//
-//
-//
-//    WECHATPAY：
-//    1313776801;SHY980;pos1:1;8468;2018-03-24 11:26:07;2018-03-24;10113;shy980-20180324-10113-oWrmq0962;
-// o1Z-rjpa7HzuYB3fn71ot9jI8qC0;10.50;;;;;;;;;0.00;10.50;0.00;10.50;1;0.00;;1;龙之梦店KFC;KFC;86013;KSHY;SHY;
-// 肯德基;百胜餐饮(沈阳)有限公司;肯德基东北市场;沈阳财务;;;2018-03-07
-//ALL,ALI,WX
+    
     public CheckBillTask(String taskid, String channel, Integer startDate, Integer endDate) {
         this.taskid = taskid;
         this.channel = channel;
